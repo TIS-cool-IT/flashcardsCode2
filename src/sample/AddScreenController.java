6ba5f60fcfd7b3e8c715c718da33fdcca933d197
Slavka Ivanicova <slavka5212@gmail.com>
2015-11-26 15:16:36 +0100

    comment

--- src/sample/AddScreenController.java
+++ src/sample/AddScreenController.java
@@ -14,6 +14,11 @@ public class AddScreenController {
 
     public void addBtnClicked() {
         // TODO vytvorit flashcardu, ulozit
+        // na vytvorenie flashcard treba skontrolovat, ci sa moze vytvorit tj. novu funkciu
+        // treba vytvorit 2 FlashcardFace -> najpr skontrolovat, ci sa mozu vytvorit tj. dalsia funkcia
+
+        // uladanie suborov FlashcardFace (text, obrazky, zvuky) -> kam dat takuto funkciu?? sem alebo Flashcard?
+
         toEditScreen(); // TODO poslat id kategorie
     }
 
